bbcc6b1dd4d6654aa92f91503d264a7b5f00f58f
Hunchul Cho <hnccho@hanafos.com>
2016-01-01 17:41:15 +0900

    환경변수 수정

--- src/main/java/io/brant/spring/logback/util/HttpUtils.java
+++ src/main/java/io/brant/spring/logback/util/HttpUtils.java
@@ -31,8 +31,8 @@ public class HttpUtils {
 
 	public static Map<String, String> getCurrentUser() {
 		Map<String, String> mockUser = new HashMap<>();
-		mockUser.put("name", "brant");
-		mockUser.put("userGroup", "STANDARD");
+		mockUser.put("name", "hchcho");
+		mockUser.put("userGroup", "ADMIN");
 		return mockUser;
 	}
 }
